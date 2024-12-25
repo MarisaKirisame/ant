4c4fd93a10597205f5410a1b26fd7ea6e77d722a
waterlens <waterlens@waterlens.moe>
2024-12-25 23:59:42 +0800

    Rename to ant; update pretty printer; remove unnecessary match

--- lib/Syntax.ml
+++ lib/Syntax.ml
@@ -33,7 +33,6 @@ and expr =
 
 and cases =
   | SwitchCtor of (ctor * expr) list * expr option
-  | SwitchInt of (int * expr) list * expr option
   | SwitchBool of (bool * expr) list * expr option
   | MatchPattern of (pattern * expr) list
 
@@ -66,9 +65,9 @@ let pp_pattern =
 
 let pp_expr =
   let fc pf ef (p, e) =
-    break 1 ^^ string "|" ^^ space ^^ pf p ^^ space ^^ string "->" ^^ nest 2 @@ break 1 ^^ ef true e
+    break 1 ^^ group @@ string "|" ^^ space ^^ pf p ^^ space ^^ string "->" ^^ break 1 ^^ nest 2 @@ ef true e
   in
-  let fm ef e inf = string "match" ^^ space ^^ ef false e ^^ space ^^ string "of" ^^ nest 2 @@ inf in
+  let fm ef e inf = string "match" ^^ space ^^ ef false e ^^ space ^^ string "with" ^^ nest 2 @@ inf in
   let fd dflt ef = match dflt with Some x -> fc (fun _ -> underscore) ef ((), x) | None -> empty in
   let rec f c (expr : expr) =
     let pp inner = if c then parens inner else inner in
@@ -77,34 +76,41 @@ let pp_expr =
     | Int n -> string (string_of_int n)
     | Float f -> string (string_of_float f)
     | Bool b -> string (string_of_bool b)
-    | Str s -> string (String.escaped s)
+    | Str s -> string (String.escaped s) |> dquotes
     | Builtin (Builtin b) -> string b
     | Var x -> string x
+    | App (fn, []) -> f c fn
     | App (fn, xs) -> f true fn ^^ space ^^ separate_map space (f true) xs |> pp
+    | CApp (Ctor (ct, _arity), []) -> string ct
     | CApp (Ctor (ct, _arity), xs) -> string ct ^^ space ^^ separate_map space (f true) xs |> pp
     | Op (op, lhs, rhs) -> f true lhs ^^ space ^^ string op ^^ space ^^ f true rhs |> pp
     | Tup xs -> separate_map (comma ^^ space) (f true) xs |> parens
     | Lam (xs, e) ->
-        align @@ string "fun" ^^ space ^^ separate_map space pp_pattern xs ^^ space ^^ string "->" ^^ nest 2 @@ break 1
+        group @@ align @@ string "fun" ^^ space ^^ separate_map space pp_pattern xs ^^ space ^^ string "->" ^^ nest 2 @@ break 1
         ^^ f true e
         |> pp
     | Arr xs -> separate_map (semi ^^ space) (f true) xs |> brackets
     | Let (BOne (x, e1), e2) ->
-        string "let" ^^ break 1
+        align @@ string "let" ^^ space
+        ^^ (group @@ align @@ nest 2 @@ pp_pattern x ^^ space ^^ string "=" ^^ space ^^ f false e1 ^^ break 1)
+        ^^ string "in" ^^ break 1 ^^ f false e2
+    | Let (BSeq e1, e2) -> align @@ f false e1 ^^ semi ^^ break 1 ^^ f false e2
+    | Let (BRec [], _) -> failwith "Empty recursive group"
+    | Let (BRec [ (x, e1) ], e2) ->
+        align @@ string "let" ^^ space ^^ string "rec" ^^ space
         ^^ group (pp_pattern x ^^ space ^^ string "=" ^^ space ^^ f false e1 ^^ break 1)
         ^^ string "in" ^^ break 1 ^^ f false e2
-    | Let (BSeq e1, e2) -> f false e1 ^^ semi ^^ break 1 ^^ f false e2
-    | Let (_, _) -> failwith "Not implemented"
+    | Let (BRec _xs, _e2) -> failwith "Not implemented"
     | If (e, e1, e2) ->
-        string "if" ^^ break 1 ^^ f true e ^^ break 1 ^^ string "then" ^^ break 1 ^^ nest 2 @@ f true e1
-        ^^ string "else" ^^ break 1 ^^ f true e2
+        group @@ align
+        @@ (group @@ string "if" ^^ nest 2 @@ break 1 ^^ f true e)
+        ^^ break 1 ^^ string "then"
+        ^^ (nest 2 @@ break 1 ^^ f true e1)
+        ^^ break 1 ^^ string "else" ^^ nest 2 @@ break 1 ^^ f true e2
     | Sel (e, field) -> f true e ^^ dot ^^ string field
     | Match (e, MatchPattern cases) -> fm f e @@ concat_map (fc pp_pattern f) cases
     | Match (e, SwitchBool (cases, default)) ->
         let g x = string @@ string_of_bool x in
-        fm f e @@ concat_map (fc g f) cases ^^ fd default f
-    | Match (e, SwitchInt (cases, default)) ->
-        let g x = string @@ string_of_int x in
         fm f e @@ concat_map (fc g f) cases ^^ fd default f
     | Match (e, SwitchCtor (cases, default)) ->
         let g x = pp_pattern (PApp (x, Some PAny)) in
