24fdb5040b7093fed77cb1440a6ba761416e2d87
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-02 10:59:25 -0700

    save

--- lib/Common.ml
+++ lib/Common.ml
@@ -2,3 +2,9 @@ exception EXN of string
 
 let panic msg = raise (EXN msg)
 let todo msg = panic ("todo: " ^ msg)
+
+let debug str f =
+  print_endline ("try " ^ str);
+  let ret = f () in
+  print_endline ("ok! " ^ str);
+  ret
