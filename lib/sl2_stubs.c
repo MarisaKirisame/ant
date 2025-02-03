bc172012bea0f1be4f296355ced910aaafd92d39
waterlens <waterlens@waterlens.moe>
2025-02-03 22:15:46 +0800

    faster alloc

--- lib/sl2_stubs.c
+++ lib/sl2_stubs.c
@@ -1,33 +1,19 @@
+#include <caml/alloc.h>
 #include <caml/custom.h>
 #include <caml/fail.h>
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 #include "hwsl2_core/sl2-inl.h"
 
-#define Sl2_val(v) (*((psl2_t *)Data_custom_val(v)))
+typedef struct {
+  sl2_t inner;
+} sl2_wrapper;
 
-void sl2_finalize(value sl2) { free(Sl2_val(sl2)); }
+#define Sl2_val(v) (((sl2_wrapper *)Data_custom_val(v))->inner)
 
-const static struct custom_operations sl2_ops = {"sl2",
-                                                 sl2_finalize,
-                                                 custom_compare_default,
-                                                 custom_hash_default,
-                                                 custom_serialize_default,
-                                                 custom_deserialize_default,
-                                                 custom_compare_ext_default,
-                                                 custom_fixed_length_default};
-
-static value make_sl2_buf() {
-  value res = caml_alloc_custom(&sl2_ops, sizeof(psl2_t), 0, 1);
-  psl2_t psl2 = aligned_alloc(16, sizeof(sl2_t));
-  if (psl2 == NULL)
-    caml_raise_out_of_memory();
-  Sl2_val(res) = psl2;
-  return res;
-}
+#define make_sl2_buf() (caml_alloc_small(sizeof(sl2_wrapper) / sizeof(value) + 1, Abstract_tag))
 
 value sl2_unit_stub(value unit) {
   CAMLparam1(unit);
@@ -62,7 +48,7 @@ value sl2_from_int_stub(value i) {
   CAMLparam1(i);
   int64_t x = Long_val(i);
   value res = make_sl2_buf();
-  sl2_mul_buf_right(Sl2_val(res), (unsigned char *)&x, 8);
+  sl2_mul_buf_right(Sl2_val(res), (unsigned char *)&x, sizeof(x));
   CAMLreturn(res);
 }
 
@@ -73,5 +59,5 @@ value sl2_from_int_stub(value i) {
 // (0) say the worst case is a hash attack, and (1) say it is hard.
 value sl2_hash_stub(value sl2) {
   CAMLparam1(sl2);
-  CAMLreturn(Val_int(Sl2_val(sl2)[0][0][0]));
+  CAMLreturn(Val_int(*(int64_t *)Sl2_val(sl2)));
 }
