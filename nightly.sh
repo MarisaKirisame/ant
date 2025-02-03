40fb7c93961a6eded696b5a7aa24dbd4ab5b90aa
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-03 15:16:01 -0700

    save

--- nightly.sh
+++ nightly.sh
@@ -1,8 +1,7 @@
 opam switch create megatron --empty || true
 opam switch megatron
-opam switch set-invariant "ocaml<5" -y
 eval $(opam env)
 opam update
 opam upgrade -y
-opam install core dune menhir ppx_deriving ppx_sexp_conv yojson core_unix batteries pprint cmdliner -y
+opam install core dune menhir ppx_deriving ppx_sexp_conv yojson core_unix batteries pprint cmdliner core_bench -y
 dune build
