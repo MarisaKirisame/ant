fbec1f8d84ecacf7398a8a86600b1e3b5c3498a5
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-04 22:34:34 -0700

    save

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -6,3 +6,4 @@ let rec gen i aux = if i == 0 then aux else gen (i - 1) (int_list_Cons i aux)
 let rec to_ocaml x = match to_ocaml_int_list x with Nil -> [] | Cons (x, l) -> x :: to_ocaml l
 let input_list = gen 30 int_list_Nil
 let _ = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr input_list))))
+let _ = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr input_list))))
