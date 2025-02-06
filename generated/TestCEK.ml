a0aa12023478253859fcf1a83aaa58c2a6acdb40
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-05 19:37:23 -0700

    refactor

--- generated/TestCEK.ml
+++ generated/TestCEK.ml
@@ -19,7 +19,7 @@ let to_ocaml_int_list x =
       let [ x0; x1 ] = Memo.splits t in
       Cons (Memo.to_int x0, x1)
 
-let rec list_incr (x0 : seq) : seq = exec_cek (pc_to_exp 12) (Dynarray.of_list [ x0 ]) (Memo.from_constructor 0) memo
+let rec list_incr (x0 : seq) : seq = exec_cek (pc_to_exp 3) (Dynarray.of_list [ x0 ]) (Memo.from_constructor 0) memo
 
 let 0 =
   add_exp (fun x ->
@@ -32,7 +32,7 @@ let 0 =
               (fun x tl ->
                 restore_env x 4 tl;
                 x.k <- value_at_depth (get_next_cont tl) x.d;
-                x.c <- pc_to_exp 5;
+                x.c <- pc_to_exp 9;
                 x)
                 x tl))
 
@@ -43,43 +43,54 @@ let 1 =
 
 let 2 =
   add_exp (fun x ->
-      x.c <- pc_to_exp 12;
+      x.c <- pc_to_exp 3;
       x)
 
 let 3 =
+  add_exp (fun x ->
+      assert_env_length x 1;
+      push_env x (Dynarray.get x.e 0);
+      x.c <- pc_to_exp 4;
+      x)
+
+let 4 =
+  add_exp (fun x ->
+      assert_env_length x 2;
+      let last = (Dynarray.get_last x.e).seq in
+      match resolve_seq x last with
+      | None -> raw_step (record_memo_exit x) memo
+      | Some (hd, tl) -> (
+          Dynarray.remove_last x.e;
+          match Word.get_value hd with
+          | 1 ->
+              x.c <- pc_to_exp 5;
+              x
+          | 2 ->
+              let [ x0; x1 ] = Memo.splits tl in
+              push_env x (value_at_depth x0 x.d);
+              push_env x (value_at_depth x1 x.d);
+              x.c <- pc_to_exp 6;
+              x))
+
+let 5 =
   add_exp (fun x ->
       assert_env_length x 1;
       push_env x (value_at_depth (Memo.from_constructor 1) x.d);
       x.c <- pc_to_exp 1;
       x)
 
-let 4 =
-  add_exp (fun x ->
-      assert_env_length x 4;
-      drop_n x 4 2 (pc_to_exp 1))
-
-let 5 =
-  add_exp (fun x ->
-      assert_env_length x 5;
-      let x1 = (pop_env x).seq in
-      let x0 = (pop_env x).seq in
-      push_env x (value_at_depth (Memo.appends [ Memo.from_constructor 2; x0; x1 ]) x.d);
-      x.c <- pc_to_exp 4;
-      x)
-
 let 6 =
   add_exp (fun x ->
-      assert_env_length x 5;
-      let sf = env_keep_last_n x 1 in
-      x.k <- value_at_depth (Memo.appends [ Memo.from_constructor 3; sf; x.k.seq ]) x.d;
-      x.c <- pc_to_exp 2;
+      assert_env_length x 3;
+      push_env x (Dynarray.get x.e 1);
+      x.c <- pc_to_exp 7;
       x)
 
 let 7 =
   add_exp (fun x ->
       assert_env_length x 4;
-      push_env x (Dynarray.get x.e 2);
-      x.c <- pc_to_exp 6;
+      push_env x (value_at_depth (Memo.from_int 1) x.d);
+      x.c <- pc_to_exp 8;
       x)
 
 let 8 =
@@ -90,48 +101,37 @@ let 8 =
           Dynarray.remove_last x.e;
           Dynarray.remove_last x.e;
           push_env x (value_at_depth (Memo.from_int (x0 + x1)) x.d);
-          x.c <- pc_to_exp 7;
+          x.c <- pc_to_exp 11;
           x
       | _ -> raw_step (record_memo_exit x) memo)
 
 let 9 =
   add_exp (fun x ->
-      assert_env_length x 4;
-      push_env x (value_at_depth (Memo.from_int 1) x.d);
-      x.c <- pc_to_exp 8;
+      assert_env_length x 5;
+      let x1 = (pop_env x).seq in
+      let x0 = (pop_env x).seq in
+      push_env x (value_at_depth (Memo.appends [ Memo.from_constructor 2; x0; x1 ]) x.d);
+      x.c <- pc_to_exp 10;
       x)
 
 let 10 =
   add_exp (fun x ->
-      assert_env_length x 3;
-      push_env x (Dynarray.get x.e 1);
-      x.c <- pc_to_exp 9;
-      x)
+      assert_env_length x 4;
+      drop_n x 4 2 (pc_to_exp 1))
 
 let 11 =
   add_exp (fun x ->
-      assert_env_length x 2;
-      let last = (Dynarray.get_last x.e).seq in
-      match resolve_seq x last with
-      | None -> raw_step (record_memo_exit x) memo
-      | Some (hd, tl) -> (
-          Dynarray.remove_last x.e;
-          match Word.get_value hd with
-          | 1 ->
-              x.c <- pc_to_exp 3;
-              x
-          | 2 ->
-              let [ x0; x1 ] = Memo.splits tl in
-              push_env x (value_at_depth x0 x.d);
-              push_env x (value_at_depth x1 x.d);
-              x.c <- pc_to_exp 10;
-              x))
+      assert_env_length x 4;
+      push_env x (Dynarray.get x.e 2);
+      x.c <- pc_to_exp 12;
+      x)
 
 let 12 =
   add_exp (fun x ->
-      assert_env_length x 1;
-      push_env x (Dynarray.get x.e 0);
-      x.c <- pc_to_exp 11;
+      assert_env_length x 5;
+      let sf = env_keep_last_n x 1 in
+      x.k <- value_at_depth (Memo.appends [ Memo.from_constructor 3; sf; x.k.seq ]) x.d;
+      x.c <- pc_to_exp 2;
       x)
 
 let () = Memo.set_constructor_degree 0 1
