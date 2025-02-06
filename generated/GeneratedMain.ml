e7dc96110c1cf0a498c240b956acb28d7f2932c9
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-06 04:20:14 -0700

    save

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -2,8 +2,19 @@ open TestCEK
 open Ant
 open Common
 
-let rec gen i aux = if i == 0 then aux else gen (i - 1) (int_list_Cons i aux)
 let rec to_ocaml x = match to_ocaml_int_list x with Nil -> [] | Cons (x, l) -> x :: to_ocaml l
-let input_list = gen 30 int_list_Nil
-let _ = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr input_list))))
-let _ = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr input_list))))
+let rec from_ocaml x = match x with [] -> int_list_Nil | xh :: xt -> int_list_Cons xh (from_ocaml xt)
+let run x = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr (from_ocaml x)))))
+let _ = run (List.init 20 (fun i -> i))
+let _ = run (List.init 20 (fun i -> 0))
+let _ = run (List.init 20 (fun i -> 0))
+
+(*
+let _ =
+  print_endline
+    (String.concat " " (List.map string_of_int (to_ocaml (list_incr (from_ocaml (List.init 20 (fun i -> i)))))))
+
+let _ =
+  print_endline
+    (String.concat " " (List.map string_of_int (to_ocaml (list_incr (from_ocaml (List.init 22 (fun i -> i - 1)))))))
+*)
--- generated/TestCEK.ml
+++ generated/TestCEK.ml
@@ -1,3 +1,5 @@
+1
+
 open Ant
 open Word
 open Memo
@@ -23,6 +25,7 @@ let rec list_incr (x0 : seq) : seq = exec_cek (pc_to_exp 2) (Dynarray.of_list [ 
 
 let 0 =
   add_exp (fun x ->
+      assert_env_length x 1;
       match resolve_seq x x.k.seq with
       | None -> raw_step (record_memo_exit x) memo
       | Some (hd, tl) -> (
@@ -30,7 +33,7 @@ let 0 =
           | 0 -> (fun x tl -> exec_done x) x tl
           | 3 ->
               (fun x tl ->
-                restore_env x 4 tl;
+                restore_env x 1 tl;
                 x.k <- value_at_depth (get_next_cont tl) x.d;
                 x.c <- pc_to_exp 9;
                 x)
@@ -107,7 +110,7 @@ let 8 =
 
 let 9 =
   add_exp (fun x ->
-      assert_env_length x 5;
+      assert_env_length x 2;
       let x1 = (pop_env x).seq in
       let x0 = (pop_env x).seq in
       push_env x (value_at_depth (Memo.appends [ Memo.from_constructor 2; x0; x1 ]) x.d);
@@ -116,13 +119,13 @@ let 9 =
 
 let 10 =
   add_exp (fun x ->
-      assert_env_length x 4;
-      drop_n x 4 2 (pc_to_exp 11))
+      assert_env_length x 1;
+      drop_n x 1 0 (pc_to_exp 11))
 
 let 11 =
   add_exp (fun x ->
-      assert_env_length x 2;
-      return_n x 2 (pc_to_exp 0))
+      assert_env_length x 1;
+      return_n x 1 (pc_to_exp 0))
 
 let 12 =
   add_exp (fun x ->
@@ -134,12 +137,12 @@ let 12 =
 let 13 =
   add_exp (fun x ->
       assert_env_length x 5;
-      let sf = env_keep_last_n x 1 in
-      x.k <- value_at_depth (Memo.appends [ Memo.from_constructor 3; sf; x.k.seq ]) x.d;
+      let keep = env_call x [ 3 ] 1 in
+      x.k <- value_at_depth (Memo.appends [ Memo.from_constructor 3; keep; x.k.seq ]) x.d;
       x.c <- pc_to_exp 1;
       x)
 
 let () = Memo.set_constructor_degree 0 1
 let () = Memo.set_constructor_degree 1 1
 let () = Memo.set_constructor_degree 2 (-1)
-let () = Memo.set_constructor_degree 3 (-4)
+let () = Memo.set_constructor_degree 3 (-1)
