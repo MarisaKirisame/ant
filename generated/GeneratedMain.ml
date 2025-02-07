4a0cc702dfadd87f81dedd83c74d7666d9829de8
Marisa Kirisame <marisa@cs.utah.edu>
2025-02-06 23:00:55 -0700

    save

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -5,9 +5,10 @@ open Common
 let rec to_ocaml x = match to_ocaml_int_list x with Nil -> [] | Cons (x, l) -> x :: to_ocaml l
 let rec from_ocaml x = match x with [] -> int_list_Nil | xh :: xt -> int_list_Cons xh (from_ocaml xt)
 let run x = print_endline (String.concat " " (List.map string_of_int (to_ocaml (list_incr (from_ocaml x)))))
-let _ = run (List.init 20 (fun i -> i))
 let _ = run (List.init 20 (fun i -> 0))
-let _ = run (List.init 20 (fun i -> 0))
+(*let _ = run (List.init 20 (fun i -> i))*)
+(*
+let _ = run (List.init 20 (fun i -> 0))*)
 
 (*
 let _ =
