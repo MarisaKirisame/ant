c896201f447461c7e6dd461e02b6f0b404cf6128
waterlens <waterlens@waterlens.moe>
2025-02-07 20:12:43 +0800

    add hash benchmark

--- bench/bench_ant.ml
+++ bench/bench_ant.ml
@@ -1,1 +1,5 @@
-let () = Intmap_bench.bench_intmap ()
+let driver intmap hash =
+  if intmap then Intmap_bench.bench_intmap ();
+  if hash then MonoidHash_bench.bench_monoid_hash ()
+
+let _ = driver (Option.is_some (Sys.getenv_opt "INTMAP")) (Option.is_some (Sys.getenv_opt "MONOIDHASH"))
