d403f2400bd49110bf1adeef4e63407e9ab678b2
Marisa Kirisame <marisa@cs.utah.edu>
2025-08-10 18:11:42 -0600

    save

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -10,9 +10,18 @@ let list_length = 100 (*todo: error when 100*)
 (* Run the test with a large input to ensure it works correctly *)
 let rec loop i =
   if i < 30 then
-    let _ = run (List.init list_length (fun i -> i)) in
+    let _ = run (List.init list_length (fun i -> i + 5)) in
     loop (i + 1)
 
+(*todo: negative number is broken*)
+let _ = print_endline "warmup..."
 let _ = loop 0
-let _ = print_endline "warmup done..."
+let _ = print_endline "warmup done!"
+let _ = print_endline "testing insert on the end..."
 let _ = run (List.init list_length (fun i -> i + 10))
+let _ = run (List.init list_length (fun i -> i + 10))
+let _ = print_endline "testing insert on the end done!"
+let _ = print_endline "testing insert on the front..."
+let _ = run (List.init list_length (fun i -> i))
+let _ = run (List.init list_length (fun i -> i))
+let _ = print_endline "testing insert on the front done!"
