7c7fa4456078b8edf483ed1c2674d5659c948da5
Marisa Kirisame <marisa@cs.utah.edu>
2025-10-31 00:56:16 -0600

    save

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -1,45 +1,12 @@
-(* Dispatches to either RunLive.exe or RunTest.exe based on the CLI argument. *)
-
-let usage = "Usage: GeneratedMain <live|test> [args...]\n- live: run RunLive executable\n- test: run RunTest executable"
-
-let candidate_paths target =
-  let base_names =
-    let lower = String.lowercase_ascii target in
-    if Sys.win32 then [ target ^ ".exe"; lower ^ ".exe"; target; lower ]
-    else [ target ^ ".exe"; target; lower; lower ^ ".exe" ]
-  in
-  let exe_dir = Filename.dirname Sys.executable_name in
-  let cwd = Sys.getcwd () in
-  let build_dir = Filename.concat cwd "_build" in
-  let default_dir = Filename.concat build_dir "default/generated" in
-  let install_dir = Filename.concat build_dir "install/default/bin" in
-  let dirs = [ exe_dir; cwd; default_dir; install_dir ] in
-  List.concat_map (fun dir -> List.map (Filename.concat dir) base_names) dirs
-
-let find_executable target = candidate_paths target |> List.find_opt Sys.file_exists
-
-let exec_child target extra_args =
-  match find_executable target with
-  | Some exe_path ->
-      let argv = Array.of_list (exe_path :: extra_args) in
-      Unix.execv exe_path argv
-  | None ->
-      prerr_endline ("Unable to locate executable for target: " ^ target);
-      exit 1
+let usage = "Usage: GeneratedMain <live|test>"
 
 let () =
   match Array.to_list Sys.argv with
-  | _ :: mode :: rest -> (
-      let target =
-        match String.lowercase_ascii mode with "live" -> Some "RunLive" | "test" -> Some "RunTest" | _ -> None
-      in
-      match target with
-      | Some target -> (
-          try exec_child target rest
-          with Unix.Unix_error (err, _, _) ->
-            prerr_endline (Printf.sprintf "Failed to execute %s: %s" target (Unix.error_message err));
-            exit 1)
-      | None ->
+  | [ _; mode ] -> (
+      match String.lowercase_ascii mode with
+      | "live" -> RunLive.run ()
+      | "test" -> RunTest.run ()
+      | _ ->
           prerr_endline usage;
           exit 1)
   | _ ->
--- generated/RunLive.ml
+++ generated/RunLive.ml
@@ -20,6 +20,24 @@ type expr =
 
 type value = OVInt of int | OVNil | OVCons of value * value [@@deriving show]
 
+let rec pp_expr fmt expr =
+  match expr with
+  | OEInt i -> Format.pp_print_int fmt i
+  | OEPlus (lhs, rhs) -> Format.fprintf fmt "(%a + %a)" pp_expr lhs pp_expr rhs
+  | OEVar i -> Format.fprintf fmt "v%d" i
+  | OEAbs body -> Format.fprintf fmt "(fun . %a)" pp_expr body
+  | OEApp (fn, arg) -> Format.fprintf fmt "(%a %a)" pp_expr fn pp_expr arg
+  | OETrue -> Format.pp_print_string fmt "true"
+  | OEFalse -> Format.pp_print_string fmt "false"
+  | OEIf (cond, thn, els) -> Format.fprintf fmt "(if %a then %a else %a)" pp_expr cond pp_expr thn pp_expr els
+  | OENil -> Format.pp_print_string fmt "[]"
+  | OECons (hd, tl) -> Format.fprintf fmt "(%a :: %a)" pp_expr hd pp_expr tl
+  | OEMatchList (target, nil_case, cons_case) ->
+      Format.fprintf fmt "(match %a with [] -> %a | Cons -> %a)" pp_expr target pp_expr nil_case pp_expr cons_case
+  | OEFix body -> Format.fprintf fmt "(fix %a)" pp_expr body
+
+let expr_to_string expr = Format.asprintf "%a" pp_expr expr
+
 let rec nat_from_int i =
   assert (i >= 0);
   if i == 0 then OZ else OS (nat_from_int (i - 1))
@@ -47,8 +65,36 @@ let rec value_to_ocaml v =
   | VNil -> OVNil
   | VCons (x, y) -> OVCons (value_to_ocaml x, value_to_ocaml y)
 
-let run x = value_to_ocaml (eval (expr_from_ocaml x) list_Nil)
+let rec pp_value fmt value =
+  match value with
+  | OVInt i -> Format.pp_print_int fmt i
+  | OVNil -> Format.pp_print_string fmt "[]"
+  | OVCons _ as cons -> (
+      let rec gather acc = function
+        | OVCons (h, t) -> gather (h :: acc) t
+        | OVNil -> `List (List.rev acc)
+        | tail -> `Improper (List.rev acc, tail)
+      in
+      let render_list fmt elems =
+        Format.pp_print_list ~pp_sep:(fun fmt () -> Format.pp_print_string fmt "; ") pp_value fmt elems
+      in
+      match gather [] cons with
+      | `List elems -> Format.fprintf fmt "[%a]" render_list elems
+      | `Improper (elems, tail) -> Format.fprintf fmt "[%a | %a]" render_list elems pp_value tail)
 
-let () =
-  print_endline (show_value (run (OEInt 42)));
-  print_endline (show_value (run (OEApp (OEAbs (OEVar 0), OEInt 42))))
+let value_to_string value = Format.asprintf "%a" pp_value value
+let eval_expression x = value_to_ocaml (eval (expr_from_ocaml x) list_Nil)
+let mapinc = OEFix (OEMatchList (OEVar 0, OEVar 0, OECons (OEPlus (OEInt 1, OEVar 1), OEApp (OEVar 3, OEVar 0))))
+
+let run () : unit =
+  print_endline "mapinc:";
+  print_endline (expr_to_string mapinc);
+  print_endline (value_to_string (eval_expression (OEInt 42)));
+  let repeat_list x =
+    let rec build n acc = if n == 0 then acc else build (n - 1) (OECons (OEInt 1, acc)) in
+    build x OENil
+  in
+  print_endline (value_to_string (eval_expression (OEApp (mapinc, repeat_list 40))));
+  print_endline (value_to_string (eval_expression (OEApp (mapinc, repeat_list 45))));
+  ignore (eval_expression (OECons (OEInt 12, OEApp (mapinc, repeat_list 45))));
+  ()
