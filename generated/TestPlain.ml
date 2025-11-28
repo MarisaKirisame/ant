adf1f25b29886b9ddb27b4c9ed0cc6f9fa9f4d70
waterlens <waterlens@waterlens.moe>
2025-11-28 13:18:42 +0800

    format

--- generated/TestPlain.ml
+++ generated/TestPlain.ml
@@ -1,3 +1,3 @@
-type int_list  = Nil| Cons of (int) * (int_list);;
+type int_list = Nil | Cons of int * int_list
 
-let rec (list_incr) = fun (x) -> (match x with (Nil) -> (Nil)| (Cons (xh, xt)) -> (Cons ((((xh)+(1))), ((list_incr) (xt)))))
+let rec list_incr = fun x -> match x with Nil -> Nil | Cons (xh, xt) -> Cons (xh + 1, list_incr xt)
