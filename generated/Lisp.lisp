16678fc3af4cab9f8e2845dd1ce07b141d46aab1
Qiu233 <lopi258@outlook.com>
2025-12-08 08:04:07 +0800

    save

--- generated/Lisp.lisp
+++ generated/Lisp.lisp
@@ -1,73 +1,78 @@
 ;; Environment = list of (int . value) pairs
-
+(
 (define null? (xs)
     (null xs)
 )
 
+(
 (define eq? (a b)
     (eq a b))
-
+(
 (define symbol? (a)
     (symbol a))
-
+(
 (define pair? (a)
     (pair a))
-
+(
 (define atom? (a)
     (atom a))
-
-(define and (a b) (if a (if b true false) false ))
-
+(
 (define else () (quote 0))
-
+(
 (define caar (xs) (car (car xs)))
+(
 (define cdar (xs) (cdr (car xs)))
+(
+(define cddar (xs) (cdr (cdr (car xs))))
+(
 (define cadar (xs) (car (cdr (car xs))))
+(
 (define caddar (xs) (car (cdr (cdr (car xs)))))
+(
 (define caddr (xs) (car (cdr (cdr xs))))
+(
 (define cadr (xs) (car (cdr xs)))
-
+(
 (define lookup (x env)
   (cond ((null? env) (error -1))
         ((eq? x (caar env)) (cdar env))
         ((quote 0) (lookup x (cdr env)))))
-
+(
 (define pairlis (xs ys env)
   (if (null? xs)
       env
       (cons (cons (car xs) (car ys))
             (pairlis (cdr xs) (cdr ys) env))))
 
-;; (defvar a '(1 2 3))
-;; (defvar a '(2 1 3))
-;; (defvar b '(7 8 9))
-
-;; (defvar e (pairlis a b '()))
-
+(
 (define isvar (x) (and (pair? x) (eq? (car x) 'var))) ;; symbol `var` is specially supported, and has no runtime behavior
-
+(
 (define evlis (exps env)
   (if (null? exps)
       '()
       (cons (eval* (car exps) env)
             (evlis (cdr exps) env))))
-
+(
 (define evcon (clauses env)
   (cond ((null? clauses) (error -2))
         ((eval* (caar clauses) env)
          (eval* (cadar clauses) env))
         (else
          (evcon (cdr clauses) env))))
-
+(
 (define eval* (exp env)
   (cond
-    ;; variable
-    ((isvar exp)
-     (lookup (cadr exp) env))
+    ;; bare number
+    ((num exp)
+     exp)
 
     ;; quoted constant: (quote x)
     ((and (pair? exp) (eq? (car exp) 'quote))
      (cadr exp))
+
+    ;; variable
+    ((isvar exp)
+     (lookup (cadr exp) env))
 
     ;; special forms built in
     ((and (pair? exp) (eq? (car exp) 'atom))
@@ -90,13 +95,26 @@
     ((and (pair? exp) (eq? (car exp) 'cond))
      (evcon (cdr exp) env))
 
-    ;; function call where operator is a symbol
-    ;; ((symbol? (car exp))
-    ;;  (eval* (cons (lookup (car exp) env)
-    ;;               (cdr exp))
-    ;;         env))
+    ;; function call where operator is a variable
+    ((and (pair? exp) (isvar (car exp)))
+      ;; (cons (lookup (cadar exp) env)
+      ;;             (cdr exp))
+     (eval* (cons (lookup (cadar exp) env)
+                  (cdr exp))
+            env)
+            
+            )
 
     ;; ( (lambda (x ...) body) arg1 arg2 ...)
+    ((and (pair? exp) (and (pair? (car exp))
+          (eq? (caar exp) 'lambda)))
+       ((defvar params (cadar exp))
+       ((defvar body   (caddar exp))
+       ((defvar args   (evlis (cdr exp) env))
+       ((defvar env*   (pairlis params args env))
+       (eval* body env*)))))
+       
+       )
     ;; ((and (pair? (car exp))
     ;;       (eq? (caar exp) 'lambda))
     ;;  (let* ((params (cadar exp))
@@ -105,6 +123,16 @@
     ;;         (env*   (pairlis params args env)))
     ;;    (eval* body env*)))
 
+    ;; ( (define f (...) body) kont )
+    ((and (pair? exp) (and (pair? (car exp))
+          (eq? (caar exp) 'define)))
+       ((defvar fname  (cadar exp))
+       ((defvar lam    (cons 'lambda (cddar exp)))
+       ((defvar env*   (cons (cons fname lam) env))
+      ;;  (error fname))))
+       (eval* (cadr exp) env*))))
+      ;;  lam)))
+       )
     ;; ( (label f (lambda (...) body)) arg1 arg2 ...)
     ;; ((and (pair? (car exp))
     ;;       (eq? (caar exp) 'label))
@@ -113,16 +141,15 @@
     ;;         (env*   (cons (cons fname (car exp)) env)))
     ;;    (eval* (cons fdef (cdr exp)) env*)))
 
+    ((and (pair? exp) (pair? (car exp)))
+     (cdar exp))
+
     (else
      (error -3))))
-
+(
 (define evalquote (fn args)
   (eval* (cons fn args) '()))
 
-;; (eval* '(cdr '(1 2 3)) '())
+(eval* '((define 0 (1) (var 1)) ( (var 0) 42 )) '())
 
-;; (eval* (cons 'cdr '('(1 2 3))) '())
-
-;; (evalquote 'cdr '('(1 2 3)))
-
-(eval* (cons 'var '(1)) (cons (cons 1 2) '()))
+))))))))))))))))))))
