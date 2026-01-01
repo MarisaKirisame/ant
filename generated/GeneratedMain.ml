b773616a760c4b34c010c7f091f80ceaa40c959a
waterlens <waterlens@waterlens.moe>
2026-01-02 00:08:32 +0800

    handle match

--- generated/GeneratedMain.ml
+++ generated/GeneratedMain.ml
@@ -4,12 +4,7 @@ let () =
   match Array.to_list Sys.argv with
   | [ _; mode ] -> (
       match String.lowercase_ascii mode with
-      | "live-simple" -> RunLiveSimple.run ()
-      | "live-left-to-right" -> RunLiveLeftToRight.run ()
-      | "live-demand-driven" -> RunLiveDemandDriven.run ()
       | "test" -> RunTest.run ()
-      | "tailrec" -> RunTailRec.run ()
-      | "hazel" -> FromHazel.run ()
       | _ ->
           prerr_endline usage;
           exit 1)
