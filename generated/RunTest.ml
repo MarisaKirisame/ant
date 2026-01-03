caee7917c5c11f64e464345b819ef2735d4f2d3b
waterlens <waterlens@waterlens.moe>
2026-01-03 17:09:54 +0800

    fix an issue

--- generated/RunTest.ml
+++ generated/RunTest.ml
@@ -6,6 +6,5 @@ module TRC = TestRegCEK
 let run () : unit =
   let memo = Ant.Memo.init_memo () in
   let memo_result = TRC.test_all memo (Memo.from_int 0) in
-  let Memo.{ words = _; step; without_memo_step; wall_time; without_memo_wall_time } = memo_result in
-  Printf.printf "memo result: step=%d, without_memo_step=%d, wall_time=%d, without_memo_wall_time=%d\n" step
-    without_memo_step wall_time without_memo_wall_time
+  let Memo.{ words = _; step; without_memo_step; } = memo_result in
+  Printf.printf "memo result: step=%d, without_memo_step=%d\n" step without_memo_step
