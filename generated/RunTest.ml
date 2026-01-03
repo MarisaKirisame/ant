7626794b96731324a7e6212478f05767c620bc06
waterlens <waterlens@waterlens.moe>
2026-01-03 17:27:50 +0800

    fix concurrency problem

--- generated/RunTest.ml
+++ generated/RunTest.ml
@@ -6,5 +6,5 @@ module TRC = TestRegCEK
 let run () : unit =
   let memo = Ant.Memo.init_memo () in
   let memo_result = TRC.test_all memo (Memo.from_int 0) in
-  let Memo.{ words = _; step; without_memo_step; } = memo_result in
+  let Memo.{ words = _; step; without_memo_step } = memo_result in
   Printf.printf "memo result: step=%d, without_memo_step=%d\n" step without_memo_step
