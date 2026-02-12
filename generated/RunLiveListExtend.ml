9be697fd50c8a39b15cd5801cb254b1ae1026058
iwei20 <ivanw8288@gmail.com>
2026-02-12 18:41:45 -0500

    fix

--- generated/RunLiveListExtend.ml
+++ generated/RunLiveListExtend.ml
@@ -4,7 +4,7 @@ module Common = RunLiveCommon
 module LC = Common.LC
 
 (* List of increasing-length prefixes of Common.random_list *)
-let random_lists_exprs =
+let (random_lists_exprs, _) =
     List.fold_right
         (fun n (res, acc) ->
             let new_acc = LC.ECons (LC.EInt n, acc) in
@@ -12,6 +12,7 @@ let random_lists_exprs =
         )
         Common.random_list
         ([], LC.ENil)
+let random_lists_exprs = List.rev random_lists_exprs
 
 let run () =
   Common.with_outchannel steps_file (fun oc ->
@@ -19,9 +20,9 @@ let run () =
       Common.LC.populate_state ();
       let memo = Ant.Memo.init_memo () in
       let eval expr = Common.eval_expression ~memo ~write_steps expr in
-      print_endline "list_extend quicksort (quicksort fixed):"
-      (* Common.quicksort_expr
+      print_endline "list_extend quicksort (quicksort expression fixed):";
+      random_lists_exprs
       |> List.iteri (fun i e ->
-          let applied = LC.EApp (e, random_list_expr) in
+          let applied = LC.EApp (Common.quicksort_expr, e) in
           Printf.printf "step %d value: %s\n" i (Common.value_to_string (eval applied)));
-      Common.write_memo_stats_json oc memo*))
+      Common.write_memo_stats_json oc memo)
